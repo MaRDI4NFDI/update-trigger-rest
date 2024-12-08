19613589517bb1cff8c0430c136e8a5be96f1721
Tim Conrad <conrad@zib.de>
2024-12-08 15:22:48 +0100

    Added a little bit more dummy functionality

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # update-trigger-rest
-A rest service to trigger updates on the portal
+A rest service to trigger updates on the portal, e.g. to get newest data from WikiData or to add a LLM generated article summary.
+
+### Examples using curl
+
+```shell
+curl -X POST -H "Content-Type: application/json" -d '{
+  "QID": "Q123",
+  "caller": "MaRDI portal"
+}' http://localhost:5000/generate_article_summary
+```
+
--- app.py
+++ app.py
@@ -3,8 +3,23 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
+    """
+    Handles GET requests to the root URL ("/").
+      - Prints the request headers to the console for debugging purposes.
+      - Returns a simple "Hello World!" message as the response.
+    """
     print( request.headers )
     return "Hello World!"
+
+@app.route('/generate_article_summary', methods=['POST'])
+def generate_article_summary():
+    """
+    Handles POST requests to the "/generate_article_summary" endpoint.
+      - Expects the client to send a JSON payload in the request body.
+      - Returns a 204 (No Content response) to indicate successful processing.
+    """
+    print( request.get_json() )
+    return '', 204
 
 if __name__ == "__main__":
     app.run()
